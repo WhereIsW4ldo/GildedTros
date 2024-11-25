47f4339d3b4d0fd70d9853ff322d7f5e8cbd9a44
waldo.wautelet <waldo.wautelet@twipemobile.com>
2024-11-25 14:02:34 +0100

    add missing files because of gitignore?

---
+++ CSharp/GildedTros.App/GildedService.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GildedTros.App;
+
+public static class GildedService
+{
+    public static void UpdateQuality(IEnumerable<Item> items, ICollection<ItemType> itemTypes)
+    {
+        foreach (var item in items)
+            UpdateQuality(item, itemTypes);
+    }
+    
+    public static Item UpdateQuality(Item item, ICollection<ItemType> itemTypes)
+    {
+        var originalQuality = item.Quality;
+        item.SellIn -= 1;
+
+        var itemType = itemTypes.FirstOrDefault(x => x.ItemNames.Contains(item.Name))
+            ?? ItemType.Default;
+        
+        var newQuality = itemType.GetQualityChangeOverTime(item);
+
+        // No changes over 50, but items already bigger than 50 okay
+        return CapQuality(item, newQuality, originalQuality);
+    }
+
+    private static Item CapQuality(Item item, int newQuality, int originalQuality)
+    {
+        switch (newQuality)
+        {
+            case > 50 when originalQuality <= 50:
+                item.Quality = 50;
+                return item;
+            case < 0:
+                item.Quality = 0;
+                return item;
+            default:
+                item.Quality = newQuality;
+                return item;
+        }
+    }
+}
---
+++ CSharp/GildedTros.App/ItemType.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace GildedTros.App;
+
+public class ItemType
+{
+    [JsonPropertyName("type")]
+    public string Type { get; set; } = string.Empty;
+
+    [JsonPropertyName("defaultQualityChangeOverTime")]
+    public int DefaultQualityChangeOverTime { get; set; } = -1;
+
+    [JsonPropertyName("examples")]
+    public IEnumerable<string> ItemNames { get; set; } = [];
+
+    [JsonPropertyName("customChange")]
+    public IDictionary<string, int> CustomChange { get; set; } = new Dictionary<string, int>();
+
+    [JsonPropertyName("customAfterSellIn")]    
+    public CustomAfterSellIn? SetAfterSellIn { get; set; }
+
+    public static ItemType Default => new() { Type = "Default", DefaultQualityChangeOverTime = -1 };
+
+    public int GetQualityChangeOverTime(Item item)
+    {
+        var normalChange = CustomChange.TryGetValue(item.SellIn.ToString(), out var change) 
+            ? change 
+            : DefaultQualityChangeOverTime;
+
+        if (SetAfterSellIn is not null && item.SellIn < 0)
+        {
+            switch (SetAfterSellIn)
+            {
+                case CustomAfterSellIn.SetToZero:
+                    return 0;
+                    break;
+            }
+        }
+
+        return item.SellIn < 0
+            ? item.Quality + normalChange * 2
+            : item.Quality + normalChange;
+    }
+    
+    
+}
+
+public enum CustomAfterSellIn
+{
+    SetToZero
+}
